5748b68d61e7d75d8659c426f4f5840d899f7e65
naopeke <naopeke@protonmail.com>
2025-02-28 17:40:05 +0100

    assertions

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,46 +1,46 @@
-// it('my first test_01', ()=>{
-//     cy.visit('/');
-//     cy.get('[data-cy="first-board"]').type('new board {enter}');
+it('my first test_01', ()=>{
+    cy.visit('/');
+    cy.get('[data-cy="first-board"]').type('new board {enter}');
 
-// })
+})
 
-// it('my first test_02', ()=>{
-//     cy.visit('/');
-//     cy.get('h2').click();
-//     cy.wait(3000);
-//     cy.get('[data-cy="add-list-input"]').clear();
-//     cy.get('[data-cy="add-list-input"]').should('be.visible').type('new list {enter}');
-// })
+it('my first test_02', ()=>{
+    cy.visit('/');
+    cy.get('h2').click();
+    cy.wait(3000);
+    cy.get('[data-cy="add-list-input"]').clear();
+    cy.get('[data-cy="add-list-input"]').should('be.visible').type('new list {enter}');
+})
 
-// it('my first test_03', ()=>{
-//     cy.visit('/');
+it('my first test_03', ()=>{
+    cy.visit('/');
 
-//     cy.contains('new board')
-//     cy.contains('.board', 'new board')
-//     cy.get('.board')
-//     cy.get('#board-1')
-//     cy.get('[data-cy="board-item"]')
-// })
+    cy.contains('new board')
+    cy.contains('.board', 'new board')
+    cy.get('.board')
+    cy.get('#board-1')
+    cy.get('[data-cy="board-item"]')
+})
 
-// it('my first test_04', ()=>{
-//     cyan.visit('/');
-//     cyan.get('[data-cy=board-item]')
-//     .first()
-//     cyan.get('[data-cy=board-item]')
-//     .last()
-//     cyan.get('[data-cy=board-item]')
-//     .eq(0)
-//     cyan.get('[data-cy=board-item]')
-//     .ne() //next
-//     cyan.get('[data-cy=board-item]')
-//     .pre() //previous
-//     cyan.get('[data-cy=board-item]')
-//     .pare() //parent
-//     cyan.get('[data-cy=board-item]')
-//     .chi() //children
-//     cyan.get('[data-cy=board-item]')
-//     .find()
-// })
+it('my first test_04', ()=>{
+    cyan.visit('/');
+    cyan.get('[data-cy=board-item]')
+    .first()
+    cyan.get('[data-cy=board-item]')
+    .last()
+    cyan.get('[data-cy=board-item]')
+    .eq(0)
+    cyan.get('[data-cy=board-item]')
+    .ne() //next
+    cyan.get('[data-cy=board-item]')
+    .pre() //previous
+    cyan.get('[data-cy=board-item]')
+    .pare() //parent
+    cyan.get('[data-cy=board-item]')
+    .chi() //children
+    cyan.get('[data-cy=board-item]')
+    .find()
+})
 
 it('interaction', ()=>{
     cy.visit('/board/1');
@@ -54,10 +54,19 @@ it('interaction', ()=>{
         .type('new card {enter}');
 })
 
-it.only('new test', ()=>{
+it('new test', ()=>{
     cy.visit('/');
     cy.get('[data-cy="star"]')
         .first()
         .click({force: true});
+
+})
+
+it.only('making assertions', ()=>{
+    cy.visit('/board/1');
+    cy.get('[data-cy=new-card]')
+        .click();
+    cy.get('[data-cy="new-card-input"]')
+        .type('bread {enter}');
 
 })
